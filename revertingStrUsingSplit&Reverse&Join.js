5572042faf64f9c069f5488738a99298543dc1f6
MsC. Indiamaris <indiamaris@gmail.com>
2022-04-13 17:43:47 +0200

    Create revertingStrUsingSplit&Reverse&Join.js

---
+++ revertingStrUsingSplit&Reverse&Join.js
@@ -0,0 +1,8 @@
+function solution(str){
+  return str.split('').reverse().join('');  
+}
+
+console.log(solution('angelanaria'))
+
+//O método split() divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array//
+
